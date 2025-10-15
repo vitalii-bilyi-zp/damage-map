5ec119fef033ce05f78942658218172c0490c2ef
belyy_vv <belyy_vv@groupbwt.com>
2025-10-15 16:57:34 +0300

    feat: registered virtual tours seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             ObjectTypeSeeder::class,
             CommunitySeeder::class,
             DamageNoteRequestSeeder::class,
+            VirtualToursSeeder::class,
         ]);
     }
 }
--- resources/js/layouts/DefaultLayout.vue
+++ resources/js/layouts/DefaultLayout.vue
@@ -44,7 +44,7 @@
 
                 <v-list-item link :to="{name: isSuperAdmin ? 'virtual-tours' : 'virtual-tours.preview'}" exact>
                     <v-list-item-icon>
-                        <v-icon>mdi-rotate-3d</v-icon>
+                        <v-icon>mdi-image-multiple</v-icon>
                     </v-list-item-icon>
 
                     <v-list-item-content>
